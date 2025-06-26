deae1f2abece50c85cb075e2d4ea91e16247cd32
naravid19 <57556702+naravid19@users.noreply.github.com>
2025-06-26 23:06:42 +0700

    Add media file settings to Django config
    Configured MEDIA_URL and MEDIA_ROOT in settings.py to support handling of uploaded media files.

--- mytree_journal/settings.py
+++ mytree_journal/settings.py
@@ -125,11 +125,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (Uploaded images, etc.)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 
 CORS_ALLOW_ALL_ORIGINS = True
