57e68e9275ef7d4c76420deffa32f7f0917669b9
naravid19 <57556702+naravid19@users.noreply.github.com>
2025-06-30 15:50:15 +0700

    Update next.config.ts

--- mytree-frontend/next.config.ts
+++ mytree-frontend/next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
+      },
+    ],
+  },
+  // เพิ่ม config อื่น ๆ ที่ต้องการ
 };
 
 export default withFlowbiteReact(nextConfig);
